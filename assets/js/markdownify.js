95ddad626b64a38fc6009ff0d205a161d4db5418
Baelyk <b43lyk@gmail.com>
2016-09-10 08:53:39 -0500

    Allows "'$, Markdown-It Plugins
    Double quotes, single quotes, and dollar signs can now be used in both
    content and reasons. Several Markdown-It plugins:
    Emoji
    Abbr
    footnote
    subscript
    superscript
    Table of contents
    Anchor
    font awesome

--- assets/js/markdownify.js
+++ assets/js/markdownify.js
@@ -1,10 +1,29 @@
-var MarkdownIt = require('markdown-it'),
-    md = new MarkdownIt();
+var Emoji = require('markdown-it-emoji');
+var Abbr = require('markdown-it-abbr');
+var Foot = require('markdown-it-footnote');
+var Sub = require('markdown-it-sub');
+var Sup = require('markdown-it-sup');
+var TContents = require('markdown-it-table-of-contents');
+var Anch = require('markdown-it-anchor');
+var FontAwesome = require('markdown-it-fontawesome');
+var Attrs = require('markdown-it-attrs');
+
+var MarkdownIt = require('markdown-it')()
+    .use(Emoji)
+    .use(Abbr)
+    .use(Foot)
+    .use(Sub)
+    .use(Sup)
+    .use(TContents)
+    .use(Anch)
+    .use(FontAwesome)
+    .use(Attrs);
+// var md = new MarkdownIt();
 var output; // = md.render("# hi");
 
 process.argv.forEach(function (val, index, array) {
     if(index == 2) {
-        output = md.render(val);
+        output = MarkdownIt.render(val);
     }
 });
 
--- modify.php
+++ modify.php
@@ -6,6 +6,16 @@ $pass = "tardiscookie";
 $database = "wiki";
 $connection = mysqli_connect($server, $user, $pass, $database);
 
+$content = $_POST["content"];
+$reason = $_POST["reason"];
+
+$content = str_replace("$", "&#36;", $content);
+$content = str_replace('"', "&quot;", $content);
+$content = str_replace("$", "&#39;", $content);
+$reason = str_replace("$", "&#36;", $reason);
+$reason = str_replace('"', "&quot;", $reason);
+$reason = str_replace("$", "&#39;", $reason);
+
 if(!$connection) {
     die("Connection failed: " . $connection->connect_error);
 } else {
@@ -13,11 +23,11 @@ if(!$connection) {
 }
 if( isset( $_POST["create"] ) ) {
     // echo "create";
-    $sql = 'INSERT INTO pages (name, content, reason) VALUES ("' . $_POST["page"] . '", "' . htmlspecialchars($_POST["content"]) . '")';
+    $sql = 'INSERT INTO pages (name, content, reason) VALUES ("' . $_POST["page"] . '", "' . htmlspecialchars($content) . '")';
     $redirectInfo = "#created";
 } else {
     // echo "update";
-    $sql = 'UPDATE pages SET content="'.htmlspecialchars($_POST["content"]).'", reason="' . $_POST["reason"] . '" WHERE name="'.$_POST["page"].'"';
+    $sql = 'UPDATE pages SET content="'.htmlspecialchars($content).'", reason="' . $reason . '" WHERE name="'.$_POST["page"].'"';
     $redirectInfo = "#updated";
 }
 
