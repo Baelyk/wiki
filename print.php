009233c92da8f28d0e5d7afd85ae2b6f662bb9f7
Baelyk <b43lyk@gmail.com>
2016-09-12 18:17:11 -0500

    Slight tweaks to print
    Changed the css of the print page.

--- print.php
+++ print.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link id="printcss" rel="stylesheet" href="assets/css/print.css" media="screen" title="Print Stylesheet">
+        <link id="printcss" rel="stylesheet" href="assets/css/print.css" title="Print Stylesheet">
         <title></title>
     </head>
     <body>
